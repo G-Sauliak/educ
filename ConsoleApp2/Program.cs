60432a8d5d73a0c1505cb9620f031185b6279a89
SMetelitsa <bosalbelarus@gmail.com>
2020-09-08 17:50:51 +0300

    emplement one more method

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,6 +4,14 @@ using System.Threading;
 
 namespace ConsoleApp2
 {
+    enum Calculation
+    {
+        addition = 1,
+        subtraction,
+        multiplication,
+        division,
+        sqrt
+    }
     class Program
     {
         /// <summary>
@@ -29,50 +37,63 @@ namespace ConsoleApp2
             }
             while (true);
         }
+        static double InputFirst()
+        {
+            Console.WriteLine("Enter the first number");
+            double a = ReadNumber();
+            return a;
+        }
+        static double InputSecond()
+        {
+            Console.WriteLine("Enter the second number");
+            double b = ReadNumber();
+            return b;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Calculator");
             while (true)
             {
+            Begin:
                 Console.WriteLine("Choose operation:\n1 - Addition;\n2 - Subtraction; \n3 - Multiplication; \n4 - Division; \n5 - Square root \n6 - Exit programm");
                 string operation = Console.ReadLine();
+                if (operation == "6")
+                {
+                    Environment.Exit(0);
+                }
+                else if (operation == "5")
+                {
+                    Console.WriteLine("Square root");
+                    Console.WriteLine("Enter the number you want to get the square root of");
+                    double q = ReadNumber(true);
+                    double sqrt = Math.Sqrt(q);
+                    Console.WriteLine($"The result is: {sqrt}");
+                    goto Begin;
+                }
+                double a = InputFirst();
+                double b = InputSecond();
+                double c;
                 switch (operation)
                 {
                     case "1":
                         Console.WriteLine("Addition");
-                        Console.WriteLine("Enter the first number");
-                        double a = ReadNumber();
-                        Console.WriteLine("Enter the second number");
-                        double b = ReadNumber();
-                        double c = a + b;
+                        c = a + b;
                         Console.WriteLine($"The result is: {c}");
                         break;
                     case "2":
                         Console.WriteLine("Substraction");
-                        Console.WriteLine("Enter the first number");
-                        double d = ReadNumber();
-                        Console.WriteLine("Enter the second number");
-                        double e = ReadNumber();
-                        double f = d - e;
-                        Console.WriteLine($"The result is: {f}");
+                        c = a - b;
+                        Console.WriteLine($"The result is: {c}");
                         break;
                     case "3":
                         Console.WriteLine("Multiplication");
-                        Console.WriteLine("Enter the first number");
-                        double k = ReadNumber();
-                        Console.WriteLine("Enter the second number");
-                        double l = ReadNumber();
-                        double m = k * l;
-                        Console.WriteLine($"The result is: {m}");
+                        c = a * b;
+                        Console.WriteLine($"The result is: {c}");
                         break;
                     case "4":
                         Console.WriteLine("Division");
-                        Console.WriteLine("Enter the first number");
-                        double o = ReadNumber();
-                        Console.WriteLine("Enter the second number");
-                        double p = ReadNumber();
-                        double r = o / p;
-                        Console.WriteLine($"The result is: {r}");
+                        c = a / b;
+                        Console.WriteLine($"The result is: {c}");
                         break;
                     case "5":
                         Console.WriteLine("Square root");
@@ -80,9 +101,6 @@ namespace ConsoleApp2
                         double q = ReadNumber(true);
                         double sqrt = Math.Sqrt(q);
                         Console.WriteLine($"The result is: {sqrt}");
-                        break;
-                    case "6":
-                        Environment.Exit(0);
                         break;
                 }
             }
